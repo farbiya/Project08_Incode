7c674664105a9d1cd924f5a63916db17643a31d2
bhavani <devibhavani326@gmail.com>
2021-06-04 09:06:30 +0100

    first commit

--- schema/schemaUser.js
+++ schema/schemaUser.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
-var userSchema = mongoose.Schema({
-	email: {
-		type: String,
-		lowercase: true,
-		trim: true,
-		unique: true,
-		required: true
+var userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userType: {
+      desc: "user roles.",
+      trim: true,
+      type: String,
+      enum: ["Admin", "User"],
+      default: "Admin",
+      required: true,
+    },
+  },
 
-	},
-	password: {
-        type: String,
-        required: true
-    }
-	Approved: {
-        type: String,
-        default: 'Admin',
-        enum: ["user", "admin"]
-       },
-},{ timestamps: { createdAt: 'created_at' }})
-
+  { timestamps: { createdAt: "created_at" } }
+);
 
 userSchema.methods = {
-	authenticate: function (password) {
-		return password === this.password;
-	},
-	getToken: function () {
-		return jwt.sign({email: this.email}, config.secret, {expiresIn: '1d'});
-	}
-}
+  authenticate: function (password) {
+    return password === this.password;
+  },
+  getToken: function () {
+    return jwt.sign({ email: this.email }, config.secret, { expiresIn: "1d" });
+  },
+};
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
